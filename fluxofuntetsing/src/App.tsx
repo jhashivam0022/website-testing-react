2740566ec8e7d307b24ceff500e4aba4e2118854
jhashivam2200 <76090863+jhashivam0022@users.noreply.github.com>
2025-03-28 17:13:25 +0530

    commit

--- fluxofuntetsing/src/App.tsx
+++ fluxofuntetsing/src/App.tsx
@@ -2,11 +2,14 @@
 import './App.css'
 
 function App() {
-
+  const onclick = () => {
+    console.log('Hello World');
+  }
 
   return (
     <>
-      <h1>Hello This a testing Project</h1>
+      <h1>Hello This is a testing Project</h1>
+      <h2>testing</h2>
     </>
   )
 }
